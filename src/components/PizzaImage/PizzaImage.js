57c7053a4d17ed6152528eae9fb928f559901380
fernandasecinaro <secinarof@gmail.com>
2021-02-18 16:02:13 -0300

    377 mitad

---
+++ src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PizzaImage from '../../assets/pizza.jpg';
+
+const pizzaImage = props => (
+  <div className="PizzaImage">
+    <img src={PizzaImage} className="PizzaImg" />
+  </div>
+);
+
+export default pizzaImage;
